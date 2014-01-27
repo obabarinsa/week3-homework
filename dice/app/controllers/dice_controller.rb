a161001ebddc0ae93ad97d3067ca533bdc35b8cc
Omobola Babarinsa <obabarinsa@uchicago.edu>
2014-01-26 23:22:32 -0600

    update 3

--- dice/app/controllers/dice_controller.rb
+++ dice/app/controllers/dice_controller.rb
@@ -1,77 +1,108 @@
 class DiceController < ApplicationController
 
-def index
+@@keep_total = 1
+@@points_score =0
+@@first_time = true
 
+def keep_total
+	@@keep_total
+end
+
+helper_method :keep_total
+
+
+def points_score
+	@@points_score
+end
+
+helper_method :points_score
+
+def first_time
+	@@first_time
+end
+
+helper_method :first_time
+
+def index
 	render 'index'
  end
 
-def initialize 
-	@roll1 = 1 + rand(6)
-	@roll2 = 1+ rand(6)
-	@rolled = @roll1 + @roll2 
+def initialize
+	if @@keep_total ==1 
+		@@first_time = true
+		@@keep_total =2	
+	end 
+
 end 
 
 def state
-	@play = 0
-	if @play == 0
-		return starting_roll
+
+	@roll1 = 1 + rand(6)
+	@roll2 = 1+ rand(6)
+	@rolled = @roll1 + @roll2
+
+	if @@first_time == true
+	  @@first_time =false
+	  starting_roll
+	  else
+	 	
+	  repeating_roll	
+	  	
  	end 
  end
 
 
 
 def starting_roll
-	@play = 1
-
+	@@keep_total +=1
+	
 	roll_1 = @roll1
 	roll_2 = @roll2
 		
 	@roll = @rolled
 	case @roll
-	when 7 ,11
-		render 'win' 
+	when 7 , 11
+		win_game
 		return
 
-	when 2 ,3, 12
-		render 'lose' 
+	when 2 , 3, 12
+		lose_game 
 		return
 
-	else
-		puts "Your goal is now  " + @roll.to_s
-		puts "Now try to get " + " " + @roll.to_s + " " + "again before you a 7."
-		@points_score = @roll
-
-	end 
-     
+	else 
+		@@points_score = @roll
+		 
+	end  
 	 render "roll"
  
  end
 
 	def repeating_roll
-		@play+=1
-	roll_1 = @roll1
-	roll_2 = @roll2
-	@roll = @rolled
 
-	case @roll
-		when @points_score
-			 render 'win'
-			 return
+	case @rolled
+		when @@points_score
+			win_game
+			return
 		when 7 
-		  render 'lose'
-		   return
-		else
-          repeating_roll
+		  lose_game
+		   return 
+           
 	end
   render 'roll'
 	
  end
 
 	def win_game
+		@@keep_total = 1
+		@@points_score =0
+		@@first_time = true
 		render "win"		
 	end
 
 	def lose_game
+		@@keep_total = 1
+		@@points_score =0
+		@@first_time = true
 		render "lose"
     end 
 		
